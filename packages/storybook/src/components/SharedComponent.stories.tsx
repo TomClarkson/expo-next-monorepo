55fd22cbed9df4989dcaddd3d1ea14eeaae9e714
Thomas Clarkson <tom@designsight.co.nz>
2020-09-26 20:49:11 +1200

    remove storybook

--- packages/storybook/src/components/SharedComponent.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import BaseComponent from "@shared/components";
-
-export default {
-  title: "Shared Component",
-  component: BaseComponent,
-};
-
-export const Basic = () => <BaseComponent />;
